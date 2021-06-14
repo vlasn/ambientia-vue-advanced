e651a0aff14fa930e4b80ff20379e1c036bc6705
vlasn <needikola@gmail.com>
2021-06-14 12:09:15 +0300

    feat: added global route guard

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -11,17 +11,7 @@ export default {
             loggedIn: false
         }
     },
-    // this is how you technically can access component instance data in a beforeEnter route hook.
-    // I would generally advise against this as you can use a different route hook - beforeRouteUpdate - instead.
-    // the beforeRouteUpdate hook is called when the route transition has been confirmed but before it has been committed and has an identical footprint
-    beforeRouteEnter(to, from, next) {
-        return next(vm => {
-            if (!vm.loggedIn) {
-                return next({name: 'login'})
-            }
-            return
-        })
-    },
+
     mounted() {
         console.log('mounted');
     }
--- src/views/index.js
+++ src/views/index.js
@@ -9,19 +9,35 @@ import Account from './Account';
 
 const storeBaseRoute = 'pood';
 
+const globalState = {
+    loggedIn: false
+};
+
+window.globalState = globalState;
+
 const routes = [
     {path: '/landing', component: Landing},
     {path: '/logi-sisse', name: 'login', component: Login},
-    {path: '/konto', name: 'account', component: Account },
+    {path: '/konto', name: 'account', component: Account, meta: { requiresAuthentication: true }},
     {path: `/${storeBaseRoute}`, name: 'store', component: Store},
     {path: `/${storeBaseRoute}/:productId`, name: 'product', component: Product},
     {path: '/cart', component: Cart, children: [
         {path: 'checkout', component: Checkout}
-    ]},
+    ], meta: { requiresAuthentication: true }},
     {path: '*', component: Landing}
 ]
-
-export default new VueRouter({
+const router = new VueRouter({
     routes,
     mode: 'history'
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.meta?.requiresAuthentication) {
+        if (!globalState.loggedIn) {
+            return next({ name: 'login' });
+        }
+    }
+    next()
+});
+
+export default router;
